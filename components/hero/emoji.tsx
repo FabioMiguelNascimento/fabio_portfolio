7c28a0ac57c9f02b23bdcc5d804173c6f562f5b9
FabioMiguelNascimento <fabiomnascimento05@gmail.com>
2026-01-11 21:24:29 -0300

    feat: enhance emoji component with animation on click

--- components/hero/emoji.tsx
+++ components/hero/emoji.tsx
@@ -1,20 +1,29 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Emoji() {
-    const [index, setIndex] = useState(1);
+  const [index, setIndex] = useState(1);
+  const [isAnimating, setIsAnimating] = useState(false);
 
   const randomEmoji = () => {
-      setIndex(Math.floor(Math.random() * 7 + 1));
+    setIsAnimating(true);
+    setIndex(Math.floor(Math.random() * 7 + 1));
+    setTimeout(() => setIsAnimating(false), 300);
   };
 
   return (
     <span className="relative flex items-center justify-center w-5 h-5 shrink-0">
       <Image
+        style={{
+          transition: "transform 0.2s cubic-bezier(0.175, 0.885, 0.32, 1.275)", // Efeito elástico (bouncy)
+          transform: isAnimating
+            ? "scale(1.5) rotate(15deg)"
+            : "scale(1) rotate(0deg)",
+        }}
         onClick={() => randomEmoji()}
-        src={`/emojis/${index}.png`} 
+        src={`/emojis/${index}.png`}
         alt="waving hand"
         fill
         sizes="20px"
